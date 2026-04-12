eb82946047cf5da181e214cb012d87e1078273d2
Ian Lam <ianklam09@gmail.com>
2026-04-12 04:20:09 -0400

    barebones experience

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import {MatDivider} from '@angular/material/divider';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatDivider
+  ],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css'
 })
 export class ExperienceComponent {
-
+  jobs = [
+    {
+      company: '84.51°',
+      role: 'Software Engineer Co-op',
+      points: [
+        'Built enterprise IAM systems using OAuth2 and Okta',
+        'Deployed microservices on Kubernetes (AKS)'
+      ]
+    },
+    {
+      company: 'Purdue Data Mine',
+      role: 'Data Scientist',
+      points: [
+        'Sports analytics for the Indiana Fever',
+        'Built data pipelines using Spark and SQL'
+      ]
+    }
+  ];
 }
